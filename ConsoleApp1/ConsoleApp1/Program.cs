64c363932c14305ad6126bbd49a1ecfe3ac473ce
Jan Żuromski <s20306@pjwstk.edu.pl>
2022-03-20 12:53:27 +0100

    Added file reading mechanism and comments to guide me through next steps

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            
+            var path = args[0];
+            // var fi = new FileInfo(path);
+            FileInfo fi = new(path);
+
+            var fileContent = new List<string>();
+            using (StreamReader stream = new(fi.OpenRead()))
+            {
+                // Analogicznie do ""
+                //string line = string.Empty;
+                string line = null;
+
+                while ((line = await stream.ReadLineAsync()) != null)
+                {
+                    fileContent.Add(line);
+                }
+
+            }
+
+            foreach (var item in fileContent)
+            {
+                Console.WriteLine(item);
+            }
+
+            // DateTime - typ dla daty: metody Parse() i TryParse()
+            // DateTime.Parse("2022-03-20")
+
+            // string.isNullOrWhiteSpace(str)
+
+            // studenci bedą przechowywani w HashSet (należy napisać comparator studentów)
+
+            // zapisywanie do pliku: StreamWriter stream... stream.WriteLine(line)
+
+            // serializacja do JSON: var json = JsonSerializer.Serializer(set)
+
         }
     }
 }

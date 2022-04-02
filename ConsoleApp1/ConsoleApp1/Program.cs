98282bd1c7114a4c7b2f3796974c02501acf8165
Jan Żuromski <zuromskijan@gmail.com>
2022-04-02 22:37:28 +0200

    Created Student Class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
 {
     public class Program
     {
+        
         public static async Task Main(string[] args)
         {
             var path = args[0];
@@ -43,6 +44,97 @@ namespace ConsoleApp1
 
             // serializacja do JSON: var json = JsonSerializer.Serializer(set)
 
+        }
+    }
+
+    class Student
+    {
+        public string Name, Surname, Faculty, CourseType, Email, FatherName, MotherName;
+        public int IndexNumer;
+        public DateTime StudiesStart;
+        public static string LoggerPath = "Data/log.txt";
+        public StreamWriter Logger;
+        
+
+        public Student(string entry)
+        {
+            Logger = new(LoggerPath, append: true);
+            string[] entrysplit = entry.Split(",");
+            for (int i = 0; i < entrysplit.Length; i++)
+            {
+                if (entrysplit[i].Equals(""))
+                {
+                    Logger.Write(entry);
+                    break;
+                }
+            }
+
+            int counter = 0;
+            Name = entrysplit[counter++];
+            Surname = entrysplit[counter++];
+            Faculty = entrysplit[counter++];
+            CourseType = entrysplit[counter++];
+            IndexNumer = int.Parse(entrysplit[counter++]);
+            StudiesStart = DateTime.Parse(entrysplit[counter++]);
+            Email = entrysplit[counter++];
+            FatherName = entrysplit[counter++];
+            MotherName = entrysplit[counter];
+            
+            Logger.Close();
+        }
+        
+    }
+
+    class OwnComparer : Comparer<Student>
+    {
+        public override int Compare(Student x, Student y)
+        {
+            // INDEX NUMBER
+            if (x.IndexNumer.CompareTo(y.IndexNumer) != 0)
+            {
+                return x.IndexNumer.CompareTo(y.IndexNumer);
+            }
+
+            // NAME
+            if (string.CompareOrdinal(x.Name, y.Name) != 0)
+            {
+                return string.CompareOrdinal(x.Name, y.Name);
+            }
+
+            // SURNAME
+            if (string.CompareOrdinal(x.Surname, y.Surname) != 0)
+            {
+                return string.CompareOrdinal(x.Surname, y.Surname);
+            }
+
+            // FACULTY
+            if (string.CompareOrdinal(x.Faculty, y.Faculty) != 0)
+            {
+                return string.CompareOrdinal(x.Faculty, y.Faculty);
+            }
+
+            // COURSE TYPE
+            if (string.CompareOrdinal(x.CourseType, y.CourseType) != 0)
+            {
+                return string.CompareOrdinal(x.CourseType, y.CourseType);
+            }
+            // STUDIES START
+            if (x.StudiesStart.CompareTo(y.StudiesStart) != 0)
+            {
+                return x.StudiesStart.CompareTo(y.StudiesStart);
+            }
+            // EMAIL
+            if (string.CompareOrdinal(x.Email, y.Email) != 0)
+            {
+                return string.CompareOrdinal(x.Email, y.Email);
+            }
+            // FATHER NAME
+            if (string.CompareOrdinal(x.FatherName, y.FatherName) != 0)
+            {
+                return string.CompareOrdinal(x.FatherName, y.FatherName);
+            }
+
+            return string.CompareOrdinal(x.MotherName, y.MotherName);
         }
     }
 }
